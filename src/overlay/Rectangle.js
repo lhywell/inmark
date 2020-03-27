f7c74b6f69bf693c9d4ae29282ba85c453e51b0b
lhywell <lihuiyin128@163.com>
2020-03-27 17:49:32 +0800

    拖拽模式下，整体拖拽包括框

--- src/overlay/Rectangle.js
+++ src/overlay/Rectangle.js
@@ -104,8 +104,7 @@ export default class RectOverlay extends Image {
                 });
             }
         })
-        
-        if (this.currShape) {
+        if (this.currShape && this.currShape.position) {
             this.setSelectedStyle(this.currShape);
         }
     }
@@ -462,11 +461,9 @@ export default class RectOverlay extends Image {
             this.currShape = shape;
 
             this.tempShape = e.target;
-
             // console.log('start', e.target.position, JSON.stringify(e.target.shape.points));
         })
         shape.on('drag', (e) => {
-
             //拖动多边形与编辑同步
             let group = shape.bound;
             group.attr({
@@ -545,6 +542,12 @@ export default class RectOverlay extends Image {
                     this.dispose();
                 }
             }
+            if (this.getDrag() === true) {
+                shape.attr({
+                    cursor: 'pointer',
+                });
+                return;
+            }
         })
         shape.on('mouseout', (e) => {
             if (this.isOpen) {
@@ -561,6 +564,43 @@ export default class RectOverlay extends Image {
             this.resetShapeStyle();
 
             this.setSelectedStyle(e.target);
+            if (this.getDrag() === true) {
+                shape.attr({
+                    draggable: false
+                })
+
+                this.cover = new zrender.Polygon({
+                    shape: {
+                        points: [
+                            [0, 0],
+                            [0, 2000],
+                            [2000, 9000],
+                            [9000, 0]
+                        ],
+                        smooth: 0,
+                    },
+                    cursor: 'pointer',
+                    draggable: true,
+                    style: {
+                        fill: 'rgba(24,151,117,0)',
+                        lineWidth: 0,
+                        lineDash: [0, 0],
+                    },
+                    zlevel: 3
+                })
+                this.zr.add(this.cover);
+                
+                this.cover.on('drag', (e) => {
+                    let array = e.target.position;
+
+                    this.group.attr({
+                        position: array
+                    });
+                })
+                this.cover.on('dragend', (e) => {
+                    this.zr.remove(this.cover);
+                })
+            }
             let shapePoints = this._toGlobal(e.target.shape.points, shape);
             const rPoints = this._changeToPoints(shapePoints);
             this._onSelected(e, {
@@ -875,6 +915,7 @@ export default class RectOverlay extends Image {
                     },
                     draggable: false
                 })
+
 
                 item.bound && item.bound.eachChild(x => {
                     x.hide();
