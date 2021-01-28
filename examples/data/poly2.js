1412ef009c1d8a10a89710b1962a463dcaa8fefc
lhywell <lihuiyin128@163.com>
2021-01-28 20:13:51 +0800

    1.增加贝塞尔曲线配置

---
+++ examples/data/poly2.js
@@ -0,0 +1,34 @@
+let data = {
+    "code": 0,
+    "data": [{
+        "coordinates": [
+            [16, 94],
+            [81, 36],
+            [146, 23],
+            [235, 30],
+            [296, 68],
+            [318, 94],
+            [296, 111],
+            [234, 62],
+            [176, 46],
+            [126, 56],
+            [78, 76],
+            [40, 106],
+            [29, 115]
+        ],
+        "notes": "123",
+        "type": "Polygon",
+        "id": "TlRZeU5EQTNMak0wTnpRMU5qVTROak0="
+    }, {
+        "coordinates": [
+            [147, 124],
+            [230, 124],
+            [230, 178],
+            [147, 178]
+        ],
+        "notes": "123",
+        "type": "Rectangle",
+        "id": "TlRZeU5EQTNMak0wTnpRMU5qVTROak0v"
+    }],
+    "message": "Success"
+}
